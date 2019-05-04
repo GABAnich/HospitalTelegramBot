b9ed30d1641417433d8fb4895db8b72e4e381aac
GABAnich <doofixxx@gmail.com>
2019-05-04 09:52:49 +0300

    Add ServicesTempInformation

---
+++ HospitalTelegramBot/HospitalTelegramBot/Model/Services/ServicesTempInformation.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalTelegramBot.Model.Services
+{
+    class ServicesTempInformation
+    {
+        public static async Task AddTempInformationAsync(int chatId, string property, string value)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                var obj = new TempInformation()
+                {
+                    UserChatId = chatId,
+                    Property = property,
+                    Value = value
+                };
+                db.TempInformation.Add(obj);
+
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public static TempInformation GetTempInformation(int chatId, string property)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                return db.TempInformation
+                    .Where(t => t.UserChatId == chatId)
+                    .Where(t => t.Property == property)
+                    .First();
+            }
+        }
+
+        public static async Task RemoveRangeTempInformationByChatIdAsync(int chatId)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                var values = db.TempInformation
+                    .Where(t => t.UserChatId == chatId);
+
+                db.TempInformation
+                    .RemoveRange(values);
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
