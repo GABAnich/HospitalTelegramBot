f9a0a2871293bb950aa642cccca370bc9777080f
GABAnich <doofixxx@gmail.com>
2019-05-07 16:08:58 +0300

    Add ServicesRecord

---
+++ HospitalTelegramBot/HospitalTelegramBot/Model/Services/ServicesRecord.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalTelegramBot.Model.Services
+{
+    class ServicesRecord
+    {
+        public static Record GetRecordById(int id)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                return db.Records
+                    .Where(r => r.Id == id)
+                    .FirstOrDefault();
+            }
+        }
+
+        public static async Task AddRecord(
+            int doctorId,
+            int userChatId,
+            string lastName,
+            string firstName,
+            string middleName,
+            string number,
+            string date)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                Record record = new Record() {
+                    DoctorId = doctorId,
+                    UserChatId = userChatId,
+                    LastName = lastName,
+                    FirstName = firstName,
+                    MiddleName = middleName,
+                    Number = number,
+                    Date = date
+                };
+                db.Records.Add(record);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
