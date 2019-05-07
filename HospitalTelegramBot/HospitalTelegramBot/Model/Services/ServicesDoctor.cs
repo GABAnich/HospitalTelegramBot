a57e1d4382cae105b3b018e7ae38570a7d0979c5
GABAnich <doofixxx@gmail.com>
2019-05-07 16:00:50 +0300

    Add Services Doctor

---
+++ HospitalTelegramBot/HospitalTelegramBot/Model/Services/ServicesDoctor.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace HospitalTelegramBot.Model.Services
+{
+    class ServicesDoctor
+    {
+        public static Doctor GetDoctorById(int id)
+        {
+            using (HospitalTelegramBotContext db = new HospitalTelegramBotContext())
+            {
+                return db.Doctors
+                    .Where(d => d.Id == id)
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
